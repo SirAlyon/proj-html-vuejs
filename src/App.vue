5a748ce15a632c9150fceea79fbb09816d8c7acb
SirAlyon <alessiosada04@gmail.com>
2022-05-13 12:45:36 +0200

    add structure and add botheader

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="container">
+        <div class="bot_head">
+          <img src="@/assets/img/classic_shop_logo2x.png" alt="" />
+          <div class="menu">
+            <a href="#">
+              Home
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <a href="#">
+              Shop
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <a href="#">
+              Products
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <a href="#">
+              Categories
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <a href="#">
+              News
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <a href="#">
+              Elements
+              <font-awesome-icon icon="fa-solid fa-angle-down" />
+            </a>
+            <span class="shop_now">
+              SHOP NOW!
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import "~/node_modules/bootstrap/scss/bootstrap.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.bot_head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+  a {
+    color: black;
+    text-decoration: none;
+    margin-right: 0.7rem;
+    margin-left: 0.7rem;
+
+  }
+  .shop_now{
+    background-color: #427ed5;
+    padding: 0.7rem 2rem;
+    border-radius: 2rem;
+    color: white;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import { faUserSecret, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(faUserSecret, faAngleDown) 
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+const bootstrap = require('bootstrap')
+Vue.use(bootstrap)
+
